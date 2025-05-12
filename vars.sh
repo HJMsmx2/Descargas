57b5099f1ba169f39ba90383ac8855a1ac85cb06
HJMsmx2 <grupo1smx2c@gmail.com>
2025-05-12 19:58:59 +0200

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -6,3 +6,9 @@ PASSsv1="usuario"
 PASSsv2="melon"
 SVIP1="192.168.237.2"     # IP fija para enp1s0
 SVIP2="192.168.1.2"       # IP fija para enp2s0
+DIR1="/home/usuario/ansibles/hosts"
+DIR2="/home/usuario/ansibles/playbooks"
+INVENTORY="$DIR1/hosts"
+PROYECTO="ADDC-HJM"
+directorio_ansible="/home/usuario/ansible_cliente"  # La ruta donde se encuentra ansible_cliente
+
