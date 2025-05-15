73f25e0ffbcbb9badfc59685f24034148abc1980
HJMsmx2 <grupo1smx2c@gmail.com>
2025-05-15 16:03:44 +0200

    Update configurar_server.sh

--- configurar_server.sh
+++ configurar_server.sh
@@ -303,21 +303,3 @@ ansible-playbook -i inventory/hosts playbook.yml
 echo "Ansible terminado"
 
 
-# Ejecutar el script en la máquina remota
-ssh root@$SVIP1 << EOF
-    echo "Conectado a $ip como $usuario"
-
-    # Cambiar al usuario especificado
-    su - usuario -c "
-
-        # Verificar si el directorio ansible_cliente existe
-        if [ -d '$directorio_ansible' ]; then
-            cd '$directorio_ansible'
-            ansible-playbook -i hosts playbook.yml
-        else
-            echo 'Directorio ansible_cliente no encontrado'
-        fi
-    "
-EOF
-
-echo "Script terminado"
