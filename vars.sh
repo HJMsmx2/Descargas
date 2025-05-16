14cb3391f21c30476777ef6aae5e8aa48c158a49
HJMsmx2 <grupo1smx2c@gmail.com>
2025-05-16 16:53:52 +0200

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -6,9 +6,21 @@ PASSsv1="usuario"
 PASSsv2="melon"
 SVIP1="192.168.237.2"     # IP fija para enp1s0
 SVIP2="192.168.1.2"       # IP fija para enp2s0
+PCIP3="192.168.1.3"
 DIR1="/home/usuario/ansibles/hosts"
 DIR2="/home/usuario/ansibles/playbooks"
 INVENTORY="$DIR1/hosts"
 PROYECTO="ADDC-HJM"
 directorio_ansible="/home/usuario/ansible_cliente"  # La ruta donde se encuentra ansible_cliente
+PLAYBOOK=(
+    "Playbook_instalaciones.yml"
+    "Playbook_KEA.yml"
+    "Playbook_squid.yml"
+    "Playbook_dns.yml"
+    "Playbooks_cliente"
+
+)
+PCIP3="192.168.1.3"
+
+
 
